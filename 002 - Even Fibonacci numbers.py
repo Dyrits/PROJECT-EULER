e3b7e57e81310658a647fe23cc6e403d0f428416
dyrits <dyrits@outlook.fr>
2020-06-25 14:35:48 +0200

    Script made more pythonic.

--- 002 - Even Fibonacci numbers.py
+++ 002 - Even Fibonacci numbers.py
@@ -5,9 +5,10 @@ def even_fibonacci_numbers(position=3, target=4000000):
     fibonacci = fibonacci_sequence.get(position - 1) + fibonacci_sequence.get(position - 2)
     fibonacci_sequence[position] = fibonacci
     if fibonacci < target:
-        return fibonacci + even_fibonacci_numbers(position + 1) if not fibonacci % 2 else even_fibonacci_numbers(position + 1)
+        return fibonacci + even_fibonacci_numbers(position + 1) if not fibonacci % 2 else even_fibonacci_numbers(
+            position + 1)
     else:
         return 0
 
 
-print(f"Answer: {even_fibonacci_numbers()}")
+print(f"Answer: {even_fibonacci_numbers()}")  # Answer: 4613732
