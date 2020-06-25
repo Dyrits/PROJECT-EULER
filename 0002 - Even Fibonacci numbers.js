c51fef1b31048c63561234c8658a274a042c9e3a
dyrits <dyrits@outlook.fr>
2020-06-25 17:51:45 +0200

    Syntax update.

--- 0002 - Even Fibonacci numbers.js
+++ 0002 - Even Fibonacci numbers.js
@@ -1,9 +1,12 @@
 const fibonacciSequence = {0: 0, 1: 1, 2: 1}
 
-function evenFinbonacciNumbers(position = 3, target = 4000000) {
-  fibonacci = fibonacciSequence[position - 1] + fibonacciSequence[position - 2]
+function evenFinbonacciNumbers(target, position = 3) {
+  let fibonacci = fibonacciSequence[position - 1] + fibonacciSequence[position - 2]
   fibonacciSequence[position] = fibonacci
-  return fibonacci < target  ? !(fibonacci % 2) ? fibonacci + evenFinbonacciNumbers(position + 1) : evenFinbonacciNumbers(position + 1) : 0;
+  if (fibonacci < target ) {
+    return !(fibonacci % 2) ?
+      fibonacci + evenFinbonacciNumbers(target, position + 1) : evenFinbonacciNumbers(target, position + 1);
+  } else { return 0; }
 }
 
-console.log("Answer: " + evenFinbonacciNumbers())  // Answer: 4613732
+console.log("Answer: " + evenFinbonacciNumbers(4000000))  // Answer: 4613732
