3db361c78b7db7c72088a9c349f965a090a94976
Dylan J. Gerrits <dyrits@outlook.com>
2024-10-07 18:25:10 +0200

    🚧 2/480

--- 0001 - Multiple of 3 and 5.js
+++ 0001 - Multiple of 3 and 5.js
@@ -1,13 +1,13 @@
 // RECURSIVE SOLUTION:
-function multiplesOf3and5(number, step3 = 3, step5 = 5, result = 0) {
-  result += step5 < number && step5 % 3 ? step5 : 0;
-  return step3 < number ? step3 + multiplesOf3and5(number, step3 + 3, step5 + 5, result) : result;
+function multiplesOf3Or5(number, step$3 = 3, step$5 = 5, result = 0) {
+  result += step$5 < number && step$5 % 3 ? step$5 : 0;
+  return step$3 < number ? step$3 + multiplesOf3Or5(number, step$3 + 3, step$5 + 5, result) : result;
 }
 
 /*
 
 // ALTERNATIVE WITH A SINGLE WHILE LOOP:
-function multiplesOf3and5(number, step3 = 0, step5 = 0, result = 0) {
+function multiplesOf3Or5(number, step3 = 0, step5 = 0, result = 0) {
   while (step3 < number) {
     result += (step3 += 3)  < number ? step3 : 0;
     result += (step5 += 5) % 3 && step5 < number ? step5 : 0;
@@ -16,7 +16,7 @@ function multiplesOf3and5(number, step3 = 0, step5 = 0, result = 0) {
 }
 
 // CLASSIC ALTERNATIVE WITH TWO FOR LOOPS:
-function multiplesOf3and5(number) {
+function multiplesOf3Or5(number) {
   let result = 0;
   for (let step3 = 3; step3 < number; step3 += 3) {
     result += step3;
@@ -29,4 +29,4 @@ function multiplesOf3and5(number) {
 
  */
 
-console.log("Answer: " + multiplesOf3and5(1000));  // Answer: 233168
+console.log("Answer: " + multiplesOf3Or5(19564));  // Answer: 233168
--- 0002 - Even Fibonacci numbers.js
+++ 0002 - Even Fibonacci numbers.js
@@ -1,12 +1,14 @@
-const fibonacciSequence = {0: 0, 1: 1, 2: 1}
+const sequence = { 0: 0, 1: 1, 2: 1 };
 
-function evenFinbonacciNumbers(target, position = 3) {
-  let fibonacci = fibonacciSequence[position - 1] + fibonacciSequence[position - 2]
-  fibonacciSequence[position] = fibonacci
-  if (fibonacci < target ) {
+function fiboEvenSum(target, position = 3) {
+  let fibonacci = sequence[position - 1] + sequence[position - 2];
+  sequence[position] = fibonacci;
+  if (fibonacci <= target) {
     return !(fibonacci % 2) ?
-      fibonacci + evenFinbonacciNumbers(target, position + 1) : evenFinbonacciNumbers(target, position + 1);
-  } else { return 0; }
+      fibonacci + fiboEvenSum(target, position + 1) : fiboEvenSum(target, position + 1);
+  } else {
+    return 0;
+  }
 }
 
-console.log("Answer: " + evenFinbonacciNumbers(4000000))  // Answer: 4613732
+console.log("Answer: " + fiboEvenSum(4000000));  // Answer: 4613732
